9c161271c110197bc3dad0b6498d10c39ef8975b
kelvinabache <kelvinabache12@gmail.com>
2025-10-02 10:25:37 -0400

    avance

--- scene_scan/lib/Home/home.dart
+++ scene_scan/lib/Home/home.dart
@@ -5,13 +5,25 @@ class Home extends StatelessWidget {
   const Home ({super.key});
 
   @override
-  Widget build(BuildContext context) {
+ Widget build(BuildContext context) {
     return Container(
-      color: Colors.black,
       width: double.infinity,
+      height: 100,       
+      decoration: const BoxDecoration(
+        color: Color.fromARGB(255, 99, 98, 98),
+        borderRadius: BorderRadius.all(
+          Radius.circular(30),
+        ),
+      ),
       child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [Text('Welcome', style: TextStyle(color: Colors.white, fontSize: 24))],
+         children: [
+              SizedBox(
+                child: IconButton(
+                icon: const Icon(Icons.add, color: Color.fromARGB(255, 8, 8, 8), size: 20),
+                onPressed: () {},
+              )
+            ),
+        ],
       ),
     );
   }
